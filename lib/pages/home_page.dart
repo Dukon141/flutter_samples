4caa7f69907167b49af2e4b138e51f365cb43687
Pedro Eduardo Quiñones García <lalogarcia1997@hotmail.com>
2025-02-15 00:12:25 -0600

    add custom button widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_samples/widgets/custom_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          padding: EdgeInsets.all(10.0),
+          children: [CustomButton(text: 'Notes', onPressed: () {})],
+        ),
+      ),
+    );
   }
 }
